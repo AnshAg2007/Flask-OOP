37e51cb01e0a26f849977e20c6801a0253fda907
Ansh  Agarwal <anshag2007@gmail.com>
2021-04-21 13:50:00 +0000

    many changes :3

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from flask import Flask 
+from flask import Flask,render_template
+import json
+import os
+import pathlib
+
+BASE = pathlib.Path(__file__).parent
+
+def get_json_data(key):
+	with open(os.path.join(BASE,"config.json"),"r") as config_file:
+		data = json.load(config_file)
+		config_file.close()
+
+	return data[key]
 
 class App(Flask):
 	def __init__(self,name):
-		super().__init__(name)
-		self.add_url_rule('/','index',self.index)
+		super(App,self).__init__(name)
+		self.add_url_rule('/','index',self.index,methods=["GET","POST"])
+		self.add_url_rule('/register','register',self.register,methods=["GET","POST"])
+		self.add_url_rule('/login','login',self.login,methods=["GET","POST"])
 	
 	def index(self):
-		return "Hello World"
+		return render_template("index.html")
+		
+	def register(self):
+		return "register"
+	
+	def login(self):
+		return "login"
 		
 	def run(self,host,port,debug=False):
 		super().run(host=host,port=port,debug=debug)
 
 if __name__ == "__main__":
 	app = App(__name__)
-	app.run(debug=True,host='0.0.0.0',port=8000)
+	app.run(host='0.0.0.0',port=8000,debug=True)
