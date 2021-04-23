30f5b03847c00deeee61ea60b8b8b7a38d51ba19
Ansh  Agarwal <anshag2007@gmail.com>
2021-04-23 14:43:41 +0000

    ummm .. i just initialized my database

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session
+from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 import pathlib
 
 BASE = pathlib.Path(__file__).parent
 
-def get_json_data(key):
-	with open(os.path.join(BASE,"config.json"),"r") as config_file:
-		data = json.load(config_file)
-		config_file.close()
-
-	return data[key]
-
 class App(Flask):
 	def __init__(self,name):
 		super(App,self).__init__(name)
+		self.config["SECRET_KEY"] = self.get_json_data("secret_key")
+		self.config["SQLALCHEMY_DATABASE_URI"] = self.get_json_data("db_uri")
+		self.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+		self.db = SQLAlchemy(self)
 		self.add_url_rule('/','index',self.index,methods=["GET","POST"])
 		self.add_url_rule('/register','register',self.register,methods=["GET","POST"])
 		self.add_url_rule('/login','login',self.login,methods=["GET","POST"])
+
+	def get_json_data(self,key):
+		with open(os.path.join(BASE,"config.json"),"r") as config_file:
+			data = json.load(config_file)
+			config_file.close()
+		return data[key]
 	
 	def index(self):
 		return render_template("index.html")
